747e84c9ef8d613e074e5030b067eeb528b72394
Dhabi966 <jalewis012003@yahoo.com>
2022-12-29 11:33:32 -0700

    added comment form and list files

---
+++ client/src/utils/auth.js
@@ -0,0 +1,45 @@
+import jwtDecode from 'jwt-decode';
+
+class AuthService {
+  getProfile() {
+    return jwtDecode(this.getToken());
+  }
+
+  loggedIn() {
+    const token = this.getToken();
+    // If there is a token and it's not expired, return `true`
+    return token && !this.isTokenExpired(token) ? true : false;
+  }
+
+  isTokenExpired(token) {
+    if (!token) return true;
+
+    try {
+      const decoded = jwtDecode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        localStorage.removeItem('id_token');
+        return true;
+      }
+    } catch (error) {
+      console.error(error);
+      return true;
+    }
+    return false;
+  }
+
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.reload();
+  }
+}
+
+export default new AuthService();
