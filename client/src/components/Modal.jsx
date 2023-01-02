afa27adbdd4f14cd698716a82818471bc74869ec
Dhabi966 <jalewis012003@yahoo.com>
2023-01-02 15:21:20 -0700

    adding connection to modal to Login/Sign up button

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -3,10 +3,12 @@ import Login from "./Login.jsx";
 import SignUp from "./SignUp.jsx";
 
 const Modal = ({ isVisible, onClose }) => {
+  console.log("isVisible:", true);
   if (!isVisible) return null;
+  
 
   const handleClose = (e) => {
-    if (e.target.id === "wrapper") onClose();
+    if (e.target.id === "wrapper" && e.target !== e.currentTarget) onClose();
   };
 
   const handleToggle = () => {
@@ -26,6 +28,13 @@ const Modal = ({ isVisible, onClose }) => {
   };
 
   return (
+    <div style={{
+        position: 'fixed',
+        zIndex: 10
+      }}
+      id="wrapper"
+      onClick={handleClose}
+    >
     <div
       className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center"
       id="wrapper"
@@ -48,7 +57,9 @@ const Modal = ({ isVisible, onClose }) => {
                 id="toggle"
                 onChange={handleToggle}
               />
-              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              <div
+                className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
+              ></div>
               <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
                 Login/Sign Up
               </span>
@@ -62,6 +73,7 @@ const Modal = ({ isVisible, onClose }) => {
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,36 +1,48 @@
-import React from "react";
 import Auth from "../utils/auth";
+import Modal from "./Modal.jsx";
+import { useState, React } from 'react';
+ 
 
 const AppNavbar = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log('handleClick called');
+    setIsModalOpen(true);
+    console.log('isModalOpen:', isModalOpen);
+
+  };
+
   return (
-    <nav className="bg-dark py-4 px-6 flex justify-between items-center">
+    <nav className="bg-dark py-0 px-0 flex justify-between items-center">
       <div className="flex items-center">
         <a href="/">
           <img
             src="/RTNavbarLogo.png"
             alt="Logo"
-            className="w-8 h-8"
+            className="w-50 h-40 max-w-60 max-h-50"
           />
-          <span className="text-black font-bold text-2xl ml-2">
+          {/* <span className="text-black font-bold text-2xl ml-2">
             Road Trippin'
-          </span>
+          </span> */}
         </a>
       </div>
       <div className="flex items-center space-x-4">
-        <a className="text-white font-bold py-2 px-4 rounded-full" href="/">
-          Plan Your Trip
+        <a className="black font-bold py-2 px-4 rounded-full" href="/">
+           Save Your Trip
         </a>
-        {/* if user is logged in show saved books and logout */}
+        {/* if user is logged in show saved trips and logout */}
         {Auth.loggedIn() ? (
           <>
             <a
-              className="text-white font-bold py-2 px-4 rounded-full"
+              className="text-black font-bold py-2 px-4 rounded-full"
               href="/saved"
             >
               See Your Trips
             </a>
             <button
-              className="text-white font-bold py-2 px-4 rounded-full"
+              className="text-black font-bold py-2 px-4 rounded-full"
               onClick={Auth.logout}
             >
               Logout
@@ -38,13 +50,15 @@ const AppNavbar = () => {
           </>
         ) : (
           <a
-            className="text-white font-bold py-2 px-4 rounded-full"
+            className="text-black font-bold py-2 px-4 rounded-full"
             href="/login"
+            onClick={handleClick}
           >
             Login/Sign Up
           </a>
         )}
       </div>
+      <Modal isVisible={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </nav>
   );
 };
