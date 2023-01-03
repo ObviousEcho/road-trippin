145c73c1d2f8c0f90eb94e3cf0a7193fae22a0b9
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-01-02 20:13:58 -0700

    added inline field

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,13 +5,25 @@ const Dashboard = () => {
     return (
         <div className='content-between'>
         <div className='justify-between content-end'>
+        
+        <form class="w-full max-w-sm">
+            <div class="flex items-center border-b border-teal-500 py-2">
+                <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Start New Trip" aria-label="Destination"></input>
+                <button class="flex-shrink-0 bg-blue-700 hover:bg-blue-800 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                    Let's Go!
+                </button>
+                <button class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button">
+                    Cancel
+                </button>
+            </div>
+        </form>
+
             <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center'>Start New Trip</button>
-        </div>
-        <div className='justify-between content-end'>
             <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-full px-5 py-2.5 text-center mr-5'>Last Trip</button>
             <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-full px-5 py-2.5 text-center mr-5'>2nd Last Trip</button>
-            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-full px-5 py-2.5 text-center mr-5'>3rd Last Trip</button>
-        </div>
+            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-full px-5 py-2.5 text-center mr-5'>3rd Last Trip
+            </button>
+            </div>
         </div>
     );
 };
