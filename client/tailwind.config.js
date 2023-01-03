4e0574c2f0bf27532b56c95a8fa953402575bf71
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-01-02 18:31:08 -0700

    further tailwindcss fixes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [client / src / pages / Dashboard.jsx],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
