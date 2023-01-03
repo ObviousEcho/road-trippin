80d37778a079a131cf0b9d487253c1fafd22f169
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-01-02 19:27:30 -0700

    tailwind changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const Dashboard = () => {
 
     return (
-        <div>
-        <div className='content-center'>
-            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white font-bold'>Start New Trip</button>
+        <div className='content-between'>
+        <div className='justify-between content-end'>
+            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center'>Start New Trip</button>
         </div>
-        <div className='content-end'>
-            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white font-bold'>Last Trip</button>
-            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white font-bold'>2nd Last Trip</button>
-            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white font-bold'>3rd Last Trip</button>
+        <div className='justify-between content-end'>
+            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-full px-5 py-2.5 text-center mr-5'>Last Trip</button>
+            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-full px-5 py-2.5 text-center mr-5'>2nd Last Trip</button>
+            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-full px-5 py-2.5 text-center mr-5'>3rd Last Trip</button>
         </div>
         </div>
     );
