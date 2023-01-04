c8d682e363c61585ed56684eb78763a9527a4884
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-01-03 17:04:09 -0700

    committing changes to dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,31 +1,65 @@
-import React from "react";
+import { React, useState } from 'react';
+// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import Map from './components/Map.tsx';
+
+function inputDestination() {
+  // store input value from form
+  const [destination, setDestination] = useState('');
+  //   const [destinationProps, setDestinationProps] = useState('');
+
+  // form input change handler
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputValue = target.value;
+    setDestination(inputValue);
+  };
+
+  // form submit handler
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!destination) {
+      return;
+    }
+    setDestinationProps(destination);
+    setDestination('');
+  };
+}
 
 const Dashboard = () => {
-
-    return (
-        <div className='content-between'>
-        <div className='justify-between content-end'>
-        
-        <form class="w-full max-w-sm">
-            <div class="flex items-center border-b border-teal-500 py-2">
-                <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Start New Trip" aria-label="Destination"></input>
-                <button class="flex-shrink-0 bg-blue-700 hover:bg-blue-800 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
-                    Let's Go!
-                </button>
-                <button class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button">
-                    Cancel
-                </button>
-            </div>
-        </form>
-
-            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center'>Start New Trip</button>
-            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-full px-5 py-2.5 text-center mr-5'>Last Trip</button>
-            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-full px-5 py-2.5 text-center mr-5'>2nd Last Trip</button>
-            <button className='flex-initial w-32 bg-blue-700 hover:bg-blue-800 text-white focus:outline-none font-medium text-sm rounded-full px-5 py-2.5 text-center mr-5'>3rd Last Trip
+  return (
+    <div>
+      <div className='text-center mt-4'>
+        <form class='w-full max-w-sm'>
+          <div class='flex items-center border-b border-teal-500 py-2'>
+            <input
+              class='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none align-middle'
+              type='text'
+              placeholder='Start New Trip'
+              aria-label='Destination'
+              value={destination}
+              onChange={handleInputChange}
+            ></input>
+            <button
+              class='flex-shrink-0 bg-blue-700 hover:bg-blue-800 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded-full'
+              type='button'
+              onClick={handleFormSubmit}
+            >
+              Let's Go!
             </button>
-            </div>
-        </div>
-    );
+            <button
+              class='flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded'
+              type='button'
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Dashboard;
