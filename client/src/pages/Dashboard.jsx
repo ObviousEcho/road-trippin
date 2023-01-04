d3c270ec15e38f4e8609bd943de1f0905fcc579c
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-01-03 21:10:03 -0700

    past trips coding commented out temporarily

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,6 @@
 import { React, useState } from 'react';
-// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// import Map from './components/Map.tsx';
+import Map from '../components/Map.tsx';
 
 // import { useParams } from 'react-router-dom';
 // import { useQuery } from '@apollo/client';
@@ -10,21 +8,21 @@ import { React, useState } from 'react';
 // import CommentList from '../components/CommentList';
 // import CommentForm from '../components/CommentForm';
 
-// import { QUERY_SINGLE_THOUGHT } from '../utils/queries';
+// import { QUERY_TRIPS } from '../utils/queries';
 
-function inputDestination() {
-  // store input value from form
+const Dashboard = () => {
+  // store input value from form - Start New Trip
   const [destination, setDestination] = useState('');
-  //   const [destinationProps, setDestinationProps] = useState('');
+  const [destinationProps, setDestinationProps] = useState('');
 
-  // form input change handler
+  // form input change handler - Start New Trip
   const handleInputChange = (e) => {
     const { target } = e;
     const inputValue = target.value;
     setDestination(inputValue);
   };
 
-  // form submit handler
+  // form submit handler - Start New Trip
   const handleFormSubmit = (e) => {
     e.preventDefault();
     if (!destination) {
@@ -33,77 +31,70 @@ function inputDestination() {
     setDestinationProps(destination);
     setDestination('');
   };
-}
 
-const Dashboard = () => {
+  // // Use `useParams()` to retrieve value of the route parameter `:profileId`
+  // const { tripsId } = useParams();
+
+  // const { loading, data } = useQuery(QUERY_TRIPS, {
+  //   // pass URL parameter
+  //   variables: { tripsId: tripsId },
+  // });
+
+  // const trips = data?.trips || {};
+
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
+
   return (
     <div>
-      <div className='text-center mt-4'>
-        <form class='w-full max-w-sm'>
-          <div class='flex items-center border-b border-teal-500 py-2'>
-            <input
-              class='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none align-middle'
-              type='text'
-              placeholder='Start New Trip'
-              aria-label='Destination'
-              value={destination}
-              onChange={handleInputChange}
-            ></input>
-            <button
-              class='flex-shrink-0 bg-blue-700 hover:bg-blue-800 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded-full'
-              type='button'
-              onClick={handleFormSubmit}
-            >
-              Let's Go!
-            </button>
-          </div>
-        </form>
+      {/* Start New Trip */}
+      <div className='grid grid-cols-2 gap-4'>
+        <div className='map'>
+          <Map destination={destinationProps} />
+        </div>
+
+        <div className='text-center mt-4'>
+          <form className='w-full max-w-sm'>
+            <div className='flex items-center border-b border-teal-500 py-2'>
+              <input
+                className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none align-middle flex items-center border-b border-teal-500'
+                type='text'
+                placeholder='Start New Trip'
+                aria-label='Destination'
+                value={destination}
+                onChange={handleInputChange}
+              ></input>
+              <button
+                className='flex-shrink-0 bg-blue-700 hover:bg-blue-800 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded-full'
+                type='button'
+                onClick={handleFormSubmit}
+              >
+                Let's Go!
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
+      {/* Past Trips */}
+      {/* <div className='my-3'>
+        <div className='bg-light py-4'>
+          <blockquote
+            className='p-4'
+            style={{
+              fontSize: '1.5rem',
+              fontStyle: 'italic',
+              border: '2px dotted #1a1a1a',
+              lineHeight: '1.5',
+            }}
+          >
+            {trips.tripname}
+          </blockquote>
+        </div>
 
-      {/* <div>
-      const SingleThought = () => {
-  // Use `useParams()` to retrieve value of the route parameter `:profileId`
-  const { thoughtId } = useParams();
-
-  const { loading, data } = useQuery(QUERY_SINGLE_THOUGHT, {
-    // pass URL parameter
-    variables: { thoughtId: thoughtId },
-  });
-
-  const thought = data?.thought || {};
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div className="my-3">
-      <h3 className="card-header bg-dark text-light p-2 m-0">
-        {thought.thoughtAuthor} <br />
-        <span style={{ fontSize: '1rem' }}>
-          had this thought on {thought.createdAt}
-        </span>
-      </h3>
-      <div className="bg-light py-4">
-        <blockquote
-          className="p-4"
-          style={{
-            fontSize: '1.5rem',
-            fontStyle: 'italic',
-            border: '2px dotted #1a1a1a',
-            lineHeight: '1.5',
-          }}
-        >
-          {thought.thoughtText}
-        </blockquote>
-      </div>
-
-      <div className="my-5">
-        <CommentList comments={thought.comments} />
-      </div>
-      <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
-        <CommentForm thoughtId={thought._id} />
-      </div>
-    </div>
+        <div className='m-3 p-4' style={{ border: '1px dotted #1a1a1a' }}>
+          <CommentForm tripsId={trips._id} />
+        </div>
       </div> */}
     </div>
   );
