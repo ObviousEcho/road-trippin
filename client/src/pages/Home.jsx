f355b5b608b290ad8e202ce57fe075561fc269d2
Dhabi966 <jalewis012003@yahoo.com>
2023-01-05 13:01:18 -0700

    adding styling changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,24 +29,52 @@ const Home = () => {
         <div className="map">
           <Map destination={destinationProps} />
         </div>
-        <section className="text-center mt-4">
-          <form>
-            <label className="text-xl">Where to?</label>
+        
+        <section
+          className="text-center mt-4"
+          style={{
+            marginTop: "-1px",
+            backgroundImage: `url('/RoadtripStickers.jpg')`,
+            backgroundPosition: "7%",
+            backgroundSize: "150%",
+            paddingRight: "60px",
+            paddingTop: "-0px",
+            backgroundClip: "content-box",
+            backgroundOrigin: "border-box",
+            height: "100%",
+          }}
+        >
+          <form
+            style={{
+              backgroundColor: "white",
+              width: "500px",
+              paddingTop: "30px",
+              paddingRight: "0px",
+              margin: "350px auto",
+              display: "block",
+              boxShadow: "0 0 50px rgba(0, 0, 0, 0.5)",
+              opacity: .95,
+            }}
+            className="mt-6"
+          >
+            <label className="text xl text-4xl font-bold">
+              Plan Your Next Road Trip!
+            </label>
             <br />
             <input
               value={destination}
               name="destination"
               type="text"
               placeholder="search"
-              className="border-4 pl-3 align-middle mt-3"
+              className="border-4 pl-3 align-middle mt-3 text-xl w-80"
               onChange={handleInputChange}
             />
             <br />
             <button
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center m-5"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-lg rounded-lg py-4 px-10 text-center m-5"
               onClick={handleFormSubmit}
             >
-              Submit
+              Let's Go
             </button>
           </form>
         </section>
